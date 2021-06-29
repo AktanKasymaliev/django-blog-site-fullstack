e58bc6afdf590ade09013061fb45f7379f2e4d74
AktanKasymaliev <aktan.kasymaliev@icloud.com>
2021-06-29 21:16:17 +0600

    add rest api

--- api/views.py
+++ api/views.py
@@ -5,3 +5,5 @@ from .serializers import CommentsSerializer
 class CommentsView(generics.CreateAPIView):
     serializer_class = CommentsSerializer
     queryset = Comments.objects.all()
+    
+# TODO улучшить вид html, сделать unnit test'ы, возможно добавить docker, сделать систему профилей
